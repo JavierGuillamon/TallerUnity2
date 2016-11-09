3cb70d1f93fe8335bb32e396181b4e7ff1feb9e0
JavierGuillamon <javier.guillamon@live.u-tad.com>
2016-11-09 22:04:24 +0100

    Implementacion sistema movimiento root motion

---
+++ Assets/PJ/PJMovementControll.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class PJMovementControll : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+    }
+    
+    //Metodo que actualiza los valores de movimiento
+    void Move()
+    {
+        //Valor de movimiento adelante y atras
+        anim.SetFloat("Forward", Input.GetAxis("Vertical"));
+        //Valor de giro izquieda y derecha
+        anim.SetFloat("Turn", Input.GetAxis("Horizontal"));
+        
+    }
+}
